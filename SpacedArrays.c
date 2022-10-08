eb247b15f63cddc567b47c4e0343e3b54f937c64
Architkak <72998453+Architkak@users.noreply.github.com>
2022-10-08 17:48:38 +0530

    Update SpacedArrays.c

--- SpacedArrays.c
+++ SpacedArrays.c
@@ -2,13 +2,13 @@
 
 int main()
 {
-    char string1[20]="Hello world";
-    char string2[20];
-    printf("String1: %s\n",string1);
+    char string_1[20]="Hello guys";
+    char string_2[20];
+    printf("String1: %s\n",string_1);
     
     for (int i=0; i<10;i++){
-        scanf("%c ",&string2[i]);
+        scanf("%c ",&string_2[i]);
     }
-    printf("String2: %s",string2);
+    printf("String2: %s",string_2);
     return 0;
 }
