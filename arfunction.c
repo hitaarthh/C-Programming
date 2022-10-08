6894ced7b2517ac5df51eeb5ebb7e952026f9cc5
Architkak <72998453+Architkak@users.noreply.github.com>
2022-10-08 17:47:40 +0530

    Update arfunction.c

--- arfunction.c
+++ arfunction.c
@@ -1,36 +1,16 @@
-#include <stdio.h>
-void search(int [],int,int);// Function prototype
+#include<stdio.h>
 int main()
-{ 
-const int SIZE=100;
-int A[SIZE],n,i,element;
-printf("Enter the limit: ");
-scanf("%d",&n);
-printf("Enter the array:\n");
-for (i=0;i<n;i++)
- { 
- scanf("%d",&A[i]);
- } 
- printf("Enter the element to be searched");
- scanf("%d",&element); 
- search(A,n,element);   // Function Call 
- return(0);
- }
- 
-void search(int A[100],int limit,int element)
 {
- int i;
- int found=0;
- for(i=0;i<limit;i++)
- {
-    if(A[i]==element)
+    int a,b,i=0;
+    scanf("%d\t%d",&a,&b);
+    while(a<=b)
     {
-       found=1;
-       break;
+        a*=3;
+        b*=2;
+        i++;
     }
- }
- if(found==1)
-  printf("Found %d at index %d",element,i);
- else
-  printf("Element not found"); 
+    printf("%d",i);
+    return(0);
 }
+
+
