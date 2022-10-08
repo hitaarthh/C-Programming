2ede8a5d55ffa1a16f9862ef8cdfa556c5b7cfd9
minusuru <115354213+minusuru@users.noreply.github.com>
2022-10-08 18:07:45 +0530

    Update SpacedArrays.c

--- SpacedArrays.c
+++ SpacedArrays.c
@@ -5,10 +5,8 @@ int main()
     char string_1[20]="Hello guys";
     char string_2[20];
     printf("String1: %s\n",string_1);
-    
-    for (int i=0; i<10;i++){
+    for (int i=0; i<10;i++)
         scanf("%c ",&string_2[i]);
-    }
     printf("String2: %s",string_2);
     return 0;
 }
