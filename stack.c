1da5551a5490ab2675cee64196487e33004ff40a
YTW7 <myahya96@gmail.com>
2022-10-08 15:50:52 +0530

    Added Program of Stack Implementation using Array & LinkedList in C Lang

---
+++ stack.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+int max=100;
+int top = -1; 
+int stack[100]; // Initializing STACK using an array
+
+// Function prototypes
+void push();       
+int pop();         
+int peek();        
+int isEmpty();   
+void traverse();
+
+
+int main(){
+    int choice,c=1;
+    while(c==1){
+        printf("\n****** \n");
+        printf("Press 1 to Push \nPress 2 to Pop \nPress 3 to Peek \nPress 4 to Check if STACK is EMPTY ");
+        printf("\nPress 5 to Traverse \nPress 0 to Exit\n");
+        scanf("%d", &choice);
+        printf("\n****** \n");
+        switch(choice){
+            case 0: c=0; break;
+            case 1: push(); break;
+            case 2: pop(); break;
+            case 3: peek(); break;
+            case 4: 
+            if(isEmpty())
+            {
+                printf("Stack is EMPTY!\n");
+            }
+            else
+            {
+                printf("Stack is NOT empty!\n");
+            }
+            break;
+            case 5: traverse(); break;
+            default: printf("Please choose a correct option!\n");
+        }
+    }
+    return 0;
+}
+
+void push()
+{// Push element to the top of the stack
+    if(top == max-1)
+        printf("STACK Overflow! \n");
+    else{
+        int ele;
+        printf("\nEnter element to be pushed into the STACK: ");
+        scanf("%d", &ele);
+        top++;
+        stack[top] = ele;
+    }
+}
+
+int pop()
+{//Remove and return the top most element of the stack
+    if(top == -1)
+        printf("STACK is EMPTY!\n");
+    else{
+        int ele = stack[top];
+        printf("%d popped out of the stack\n", ele);
+        top--;
+        return ele;
+    }
+    return -1;
+}
+
+int peek()
+{//Return the top most element of the stack
+    int ele = stack[top];
+    printf("%d is the top most element of the stack\n", ele);
+    return ele;
+}
+
+int isEmpty()
+{//Check if the stack is EMPTY or not
+    if(top == -1){
+        return 1;
+    }
+    return 0;
+}
+
+void traverse()
+{//Printing all STACK elements
+  if(top!=-1)
+  { for(int i=0;i<=top;i++)
+    {
+      printf("%d ",stack[i]);
+    }
+  }
+    printf("\n");
+}
+
---
+++ stackUsingLinkedList.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node //Creating structure of our linked list
+{
+    int data;
+    struct node* next;
+};
+struct node* start=NULL; //Globally declaring start node
+//Declaring Functions
+void push(int ele);
+void pop();
+void peek();
+void traverse();
+
+void main()  //Main Method Begins
+{   int c=1; int choice;
+    while(c==1)
+    {
+    printf("\nEnter 1 to Push, 2 to Pop, 3 to Peek, 4 to Traverse, 0 to Exit...");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+    case 0:
+        c=0;
+        break;
+    case 1:
+        printf("\nEnter element to be pushed in STACK:");
+        int ele;
+        scanf("%d",&ele);
+        push(ele);
+        break;
+    case 2:
+        pop();
+        break;
+    case 3:
+        peek();
+        break;
+    case 4:
+        traverse();
+        break;
+    default:
+        break;
+    }//Switch-Case Ends
+    }//While-Loop Ends
+}// Main Function Ends
+void push(int x)
+{   struct node* temp;
+    temp=(struct node*)malloc(sizeof(struct node));//Memory Allocation for temp Node
+    temp->data=x;
+    if(temp==NULL)
+    {   start=temp;
+        temp->next=NULL;
+    }
+    else
+    {   temp->next=start;
+        start=temp;
+    }
+}
+void pop()
+{
+   if(start==NULL)
+   { printf("\nSTACK is EMPTY!");
+   }
+   else
+   { printf("\n%d is popped out of STACK!",start->data);
+    start=start->next;
+   }
+}
+void peek()
+{if(start==NULL)
+   { printf("\nSTACK is EMPTY!");
+   }
+   else
+   { printf("\n%d is the top element!",start->data);
+   }
+
+}
+
+void traverse()
+{   if(start==NULL)
+   { printf("\nSTACK is EMPTY!");
+   }
+   else
+   { struct node* temp;
+     temp=(struct node*)malloc(sizeof(struct node));
+     temp=start;
+     while(temp!=NULL)
+     {
+         printf(" %d |",temp->data);
+         temp=temp->next;
+     }
+   }
+}
