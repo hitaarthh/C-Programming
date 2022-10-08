4b7b93ab2c9dca6cb81b41a034341e18643aa20c
vedikavivek <115354700+vedikavivek@users.noreply.github.com>
2022-10-08 18:23:49 +0530

    Create conditionals.c

---
+++ conditionals.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+int a=0, b=1, c=2, d=4, z;
+int x=3&&4;
+printf("%d\n",x);
+if (z=a|b|c&&d){printf("Tennis\n");}
+else{printf("Football\n");}
+printf("%d",z);
+return 0;}
