11e997d73a6206bd00e584a1c81f8008a1f3ebd0
luciffer077 <115617777+luciffer077@users.noreply.github.com>
2022-10-12 13:12:03 +0530

    Update Assigning_if.c

--- Assigning_if.c
+++ Assigning_if.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 int main(){
 int i;
-if (i=0,2,3){
-	printf("Rani Lakshmi Bhai\n");}
+if (i=0,2,3)
+	printf("Rani Lakshmi Bhai\n");
 else{ printf("Nivedita\n");}
 printf("%d\n",i);
 }
