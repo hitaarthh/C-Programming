9a6b3f10117a01597de45cf327bb77e27c5e312c
Maniish S <111979203+Maniish01@users.noreply.github.com>
2022-10-22 22:39:04 +0530

    factorial

--- factorial.c
+++ factorial.c
@@ -1,30 +1,11 @@
 #include <stdio.h>
 
-// Recursive method
-
-int factorial_recur(int num) {
-	if (num == 0 || num == 1)
-		return 1;
-	return num * factorial_recur(num - 1);
-}
-
-
-// Iterative method
-
-int factorial(int num) {
-	int fac = 1;
-	for (int x = 2; x <= num; x++)
-		fac = fac * x;
-	return fac;
-}
-
-int main()
-{
-	int num = 5;
-
-	int fac = factorial(5);
-
-	printf("Factorial of %d is %d", num, fac);
-
-	return 0;
+int main(){
+    int a,i,b;
+    scanf("%d",&a);
+    b=1;
+    for(i=1;i<=a;i++){
+        b=b*i;
+    }
+    printf("%d",b);
 }
